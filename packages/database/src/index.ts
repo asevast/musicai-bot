014d13f868a9baca607c92a574d1a38a0c2bf8b2
asevast <asevast@musicai.local>
2026-04-06 11:24:07 +0300

    fix: Prisma client export for monorepo

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -12,4 +12,4 @@ export const prisma =
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-export * from '@prisma/client';
+export type { PrismaClient } from '@prisma/client';
