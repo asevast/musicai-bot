2d6b79ecb3af858951877809dbd99865c289e4cc
asevast <asevast@musicai.local>
2026-04-06 11:30:19 +0300

    fix: skip build in CI, just generate Prisma

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from './generated/client';
+import { PrismaClient } from './generated/client/index.js';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -12,4 +12,4 @@ export const prisma =
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-export type { PrismaClient } from './generated/client';
+export type { PrismaClient } from './generated/client/index.js';
